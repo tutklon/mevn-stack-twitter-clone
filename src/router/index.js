a2ac44df471146a2004e5f695fc9451717f0d1b9
tutklon <mettheus0@gmail.com>
2022-02-07 16:58:01 +0300

    add: tailwind.css and configurations

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,16 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/views/Home.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    }
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
